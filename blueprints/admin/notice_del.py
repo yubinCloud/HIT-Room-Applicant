35a5f12950eba65c95f5ccbc925afcfead6e8ec7
lvyyevd <1742365379@qq.com>
2020-04-27 00:23:00 +0800

    room_apply

---
+++ blueprints/admin/notice_del.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, jsonify, session, request
+
+from hitapply.models import Notice
+
+
+notice_del = Blueprint('notice_del', __name__)
+
+@notice_del.route('', methods = ['POST'])
+def Notice_del(notice_id):
+    account = session.get('admin_login')
+    if account is None:
+        return jsonify(code=-102, data={"tip": "用户未登录"})
+
+    json_data = request.get_json(silent=True)
+
+    if json_data is None:
+        return jsonify(code=-101, data=None)
+    notice = Notice.query.get(notice_id=notice_id)
+    try:
+        db.session.delete(notice)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        return jsonify(code=101, data={'error': '数据库异常'})
+
+    return jsonify(code=0, data={'tip': ''})
