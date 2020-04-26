2b9099e177c81ed5605833db0bbb0d68f260ab16
lvyyevd <1742365379@qq.com>
2020-04-27 00:22:42 +0800

    room_apply

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from hitapply.extensions import db
 from hitapply.settings import config
 from hitapply.blueprints.stu import time_get, notice_list, stu_apply, room_floor_get, stu_room_use, notice_info, \
     stu_apply_info, room_info, room_use_info
-from hitapply.blueprints.admin import admin_login, admin_logout, user_info
+from hitapply.blueprints.admin import admin_login, admin_logout, user_info, notice_add, notice_del
 
 
 
@@ -91,3 +91,11 @@ def register_blueprints(app):
     # http://xx.com/api/admin/user
     # GET: 获取用户信息
     app.register_blueprint(user_info.user_info, url_prefix='/api/admin/user')
+
+    # http://xx.com/api/admin/notice
+    # POST: 新增公告
+    app.register_blueprint(notice_add.notice_add, url_prefix='/api/admin/notice')
+
+    # http://xx.com/api/admin/notice/id
+    # POST: 删除公告
+    app.register_blueprint(notice_del.notice_del, url_prefix='/api/admin/notice/<int:notice_id>')
