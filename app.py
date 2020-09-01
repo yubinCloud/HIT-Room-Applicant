815485ce04ad308b6202d35a6d47006aa166e543
yubinCloud <yubin_SkyWalker@yeah.net>
2020-09-01 17:16:59 +0800

    将后端代码全部存入src中

--- app.py
+++
@@ -1,6 +0,0 @@
-from hitapply import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run()
--- blueprints/admin/admin_logout.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify, session
-
-admin_logout = Blueprint('admin_logout', __name__)
-
-@admin_logout.route('', methods = ['POST'])
-def Admin_Logout():
-    session.pop('admin_login', None);
-    return jsonify(code=0, data={"tip": "已退出登录"})
