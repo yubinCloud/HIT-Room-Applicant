038f60a0df5c2f82f12af7f818227549fce2a842
yubinCloud <yubin_SkyWalker@yeah.net>
2020-09-21 07:56:29 +0800

    a little change

--- endsrc/blueprints/admin/admin_logout.py
+++ endsrc/blueprints/admin/admin_logout.py
@@ -4,5 +4,5 @@ admin_logout = Blueprint('admin_logout', __name__)
 
 @admin_logout.route('', methods = ['POST'])
 def Admin_Logout():
-    session.pop('admin_login', None);
+    session.pop('admin_login', None)
     return jsonify(code=0, data={"tip": "已退出登录"})
