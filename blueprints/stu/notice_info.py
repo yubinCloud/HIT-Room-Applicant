b635d66338fe63451f7201b78e2c2d150dc731e1
XQL <1203688355@qq.com>
2020-09-23 20:38:46 +0800

    xql branch

--- blueprints/stu/notice_info.py
+++ blueprints/stu/notice_info.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify
-
 from models import Notice
 
 notice_info = Blueprint('notice_info', __name__)
-@notice_info.route('', methods = ['GET'])
 
+
+@notice_info.route('', methods=['GET'])
 def Notice_Info(notice_id):
     if notice_id == 0:
         length = len(Notice.query.all())
@@ -23,7 +23,7 @@ def Notice_Info(notice_id):
     else:
         record = Notice.query.get(notice_id)
         if record is None:
-            return jsonify(code = -102, data = {notice_id})
+            return jsonify(code=-102, data={notice_id})
         else:
             data = {
                 'id': record.notice_id,
--- blueprints/stu/notice_list.py
+++ blueprints/stu/notice_list.py
@@ -2,16 +2,17 @@ from flask import Blueprint, jsonify, request
 from models import Notice
 
 notice_list = Blueprint('notice_list', __name__)
-@notice_list.route('', methods = ['GET'])
 
+
+@notice_list.route('', methods=['GET'])
 def Notice_List():
     start_id = request.args.get('startid')
     end_id = request.args.get('endid')
     data = []
-    record = Notice.query.order_by(Notice.time.desc()).slice(int(start_id), int(end_id)+1).all()
+    record = Notice.query.order_by(Notice.time.desc()).slice(int(start_id), int(end_id) + 1).all()
 
     if record is None:
-        return jsonify(error_code = -102, data = {})
+        return jsonify(error_code=-102, data={})
 
     for i in record:
         temp = {
