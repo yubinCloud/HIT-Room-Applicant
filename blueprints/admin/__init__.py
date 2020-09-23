4784df183813a90949fcb5a8e7fa55a45ba89134
yubinCloud <yubin_SkyWalker@yeah.net>
2020-09-23 21:06:51 +0800

    更换了admin_modify的位置

--- blueprints/admin/__init__.py
+++ blueprints/admin/__init__.py
@@ -1,4 +1,4 @@
-from .account_manage import account_about
+from .account_manage import account_about, admin_modify
 from .notice_manage import admin_notice_bp, notice_del
-from .login_manage import admin_user, admin_login, admin_logout, admin_modify
+from .login_manage import admin_user, admin_login, admin_logout
 from .room_manage import adm_room, adm_myroom
