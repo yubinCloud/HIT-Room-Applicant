5fb0bc94cffdb8a49c0a01494cdaf58770853136
XQL <1203688355@qq.com>
2020-10-19 07:55:59 +0800

    解决修改时间表的bug

--- blueprints/admin/timetable_manage/admin_timetable.py
+++ blueprints/admin/timetable_manage/admin_timetable.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request
 from common.utils import adm_login_required, send_json, record_exception
 from models import Timetable
 from extensions import db
+from common.utils import turn_to_int, turn_to_string
 
 
 admin_timetable = Blueprint('admin_timetable', __name__)
@@ -17,13 +18,13 @@ def adm_timetable_handler():
     """
     if request.method == 'GET':
         records = Timetable.query.all()
-        result_data = [['第{}节'.format(record.class_id), record.begin_time, record.end_time]
+        result_data = [['第{}节'.format(record.class_id), turn_to_string(record.begin_time), turn_to_string(record.end_time)]
                        for record in records]
         return send_json(0, result_data)
 
     # 当 method 为 POST 时
     else:
-        new_timetable = request.get_json(silent=True).get('timetable_manage')
+        new_timetable = request.get_json(silent=True).get('timetable')
         if new_timetable is None:
             return {-101, '缺少必要参数'}
 
@@ -33,7 +34,11 @@ def adm_timetable_handler():
             db.session.delete(klass)
         # 加入新时间表
         for klass in new_timetable:
-            new_record = Timetable(klass[0], klass[1], klass[2])
+            new_record = Timetable()
+            new_record.class_id = klass[0]
+            new_record.begin_time = turn_to_int(klass[1])
+            new_record.end_time = turn_to_int(klass[2])
+            # new_record = Timetable(klass[0], turn_to_int(klass[1]), turn_to_int(klass[2]))
             db.session.add(new_record)
         # 提交到数据库
         try:
